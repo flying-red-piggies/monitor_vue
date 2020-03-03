0e801296659b8f8181430aa94ac99091c7a4ba50
kitfrankenstein <921518339@qq.com>
2020-03-03 20:10:56 +0800

    修改一些图表

--- src/components/statistic/EcsMonitor.vue
+++ src/components/statistic/EcsMonitor.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="ecsMonitor">
-    <el-form-item label="选择实例"/>
     <el-select v-model="instanceId" placeholder="请选择"
                @change="refreshData(instanceId)"
     style="margin-bottom: 20px;">
@@ -11,21 +10,21 @@
       </el-option>
     </el-select>
 <!--    cpu三个一组-->
-    <div class="titleTab">cpu</div>
     <el-card class="box-card chartGroup">
+      <div class="titleTab">CPU</div>
       <div>
         <div class="group1">
-          <div id="cpuTotalChart" style="width: 800px;height: 300px;"></div>
+          <div id="cpuTotalChart" style="width: 100%;height: 300px;"></div>
         </div>
         <div class="group2">
-          <div id="cpuSystemChart" style="width: 400px;height: 300px;"></div>
-          <div id="cpuUserChart" style="width: 400px;height: 300px;"></div>
+          <div id="cpuSystemChart" style="width: 50%;height: 300px;"></div>
+          <div id="cpuUserChart" style="width: 50%;height: 300px;"></div>
         </div>
       </div>
     </el-card>
 <!--    -->
-    <div class="titleTab">系统负载</div>
     <el-card shadow="always" class="box-card">
+      <div class="titleTab">系统负载</div>
       <div class="group2">
         <div id="load1MChart" style="width: 800px;height:300px;"></div>
       </div>
@@ -37,16 +36,16 @@
 
 <!--    -->
 <!--    memory自己一组-->
-    <div class="titleTab">内存</div>
     <el-card class="box-card chartGroup">
-    <div>
-      <div id="memoryChart" style="width: 800px;height:300px;"></div>
-    </div>
+      <div class="titleTab">内存</div>
+      <div>
+        <div id="memoryChart" style="width: 800px;height:300px;"></div>
+      </div>
     </el-card>
 <!--    -->
 <!--    disk五个一组-->
-    <div class="titleTab">硬盘</div>
     <el-card class="box-card chartGroup">
+      <div class="titleTab">硬盘</div>
       <div>
         <div class="group1">
           <div id="diskInodeChart" style="width: 800px;height:300px;"></div>
@@ -91,15 +90,13 @@ import ecsMetric from '@/api/ecs/ecsMetric'
 import moment from 'moment'
 export default {
   name: 'ecsMonitor',
-  // mounted () {
-  //   this.myChart = this.$echarts.init(document.getElementById(chartId))
-  // },
+  mounted () {
+    this.initCharts()
+    this.showLoading()
+  },
   created () {
     ecsInfo.getInstances().then(res => {
-      this.cpuTotal = this.$echarts.init(document.getElementById('cpuTotalChart'))
-      this.cpuSystem = this.$echarts.init(document.getElementById('cpuSystemChart'))
-      this.cpuUser = this.$echarts.init(document.getElementById('cpuUserChart'))
-
+      console.log(res.data)
       this.instances = res.data
       this.instanceId = res.data[0].instanceId
       this.refreshData(this.instanceId)
@@ -111,73 +108,288 @@ export default {
       instanceId: '',
       cpuTotal: {},
       cpuSystem: {},
-      cpuUser: {}
+      cpuUser: {},
+      load1M: {},
+      load5M: {},
+      load15M: {},
+      memory: {},
+      diskInode: {},
+      diskRRate: {},
+      diskWRate: {},
+      diskRIops: {},
+      diskWIops: {},
+      tcpConnection: {},
+      netIRate: {},
+      netORate: {},
+      netIPps: {},
+      netOPps: {},
+      getCpu: (instanceId, startTime, endTime, interval) => {
+        return new Promise((resolve, reject) => {
+          ecsMetric.getCpuTotal(instanceId, startTime, endTime, interval).then((res) => resolve(res))
+        })
+      }
     }
   },
   methods: {
+    initCharts () {
+      this.cpuTotal = this.$echarts.init(document.getElementById('cpuTotalChart'))
+      this.cpuSystem = this.$echarts.init(document.getElementById('cpuSystemChart'))
+      this.cpuUser = this.$echarts.init(document.getElementById('cpuUserChart'))
+      this.load1M = this.$echarts.init(document.getElementById('load1MChart'))
+      this.load5M = this.$echarts.init(document.getElementById('load5MChart'))
+      this.load15M = this.$echarts.init(document.getElementById('load15MChart'))
+      this.memory = this.$echarts.init(document.getElementById('memoryChart'))
+      this.diskInode = this.$echarts.init(document.getElementById('diskInodeChart'))
+      this.diskRRate = this.$echarts.init(document.getElementById('diskRRateChart'))
+      this.diskWRate = this.$echarts.init(document.getElementById('diskWRateChart'))
+      this.diskRIops = this.$echarts.init(document.getElementById('diskRIopsChart'))
+      this.diskWIops = this.$echarts.init(document.getElementById('diskWIopsChart'))
+      this.tcpConnection = this.$echarts.init(document.getElementById('tcpConnectionChart'))
+      this.netIRate = this.$echarts.init(document.getElementById('netIRateChart'))
+      this.netORate = this.$echarts.init(document.getElementById('netORateChart'))
+      this.netIPps = this.$echarts.init(document.getElementById('netIPpsChart'))
+      this.netOPps = this.$echarts.init(document.getElementById('netOPpsChart'))
+    },
+    showLoading () {
+      this.cpuTotal.showLoading({
+        text: '正在加载'
+      })
+      this.cpuSystem.showLoading({
+        text: '正在加载'
+      })
+      this.cpuUser.showLoading({
+        text: '正在加载'
+      })
+      this.load1M.showLoading({
+        text: '正在加载'
+      })
+      this.load5M.showLoading({
+        text: '正在加载'
+      })
+      this.load15M.showLoading({
+        text: '正在加载'
+      })
+      this.memory.showLoading({
+        text: '正在加载'
+      })
+      this.diskInode.showLoading({
+        text: '正在加载'
+      })
+      this.diskRRate.showLoading({
+        text: '正在加载'
+      })
+      this.diskWRate.showLoading({
+        text: '正在加载'
+      })
+      this.diskRIops.showLoading({
+        text: '正在加载'
+      })
+      this.diskWIops.showLoading({
+        text: '正在加载'
+      })
+      this.tcpConnection.showLoading({
+        text: '正在加载'
+      })
+      this.netIRate.showLoading({
+        text: '正在加载'
+      })
+      this.netORate.showLoading({
+        text: '正在加载'
+      })
+      this.netIPps.showLoading({
+        text: '正在加载'
+      })
+      this.netOPps.showLoading({
+        text: '正在加载'
+      })
+    },
+    mapDataPoints (resData, dataNames) {
+      let dataPoints = []
+      for (let i = 0; i < dataNames.length; i++) {
+        dataPoints.push(resData.map(function (item) {
+          return item[dataNames[i]]
+        }))
+      }
+      return dataPoints
+    },
+    mapXTimes (resData) {
+      return resData.map(function (item) {
+        return moment(item['timestamp']).format('YYYY/MM/DD HH:mm')
+      })
+    },
     refreshData (instanceId) {
+      this.showLoading()
       let endTime = new Date().valueOf()
       let startTime = moment(endTime).add(-50, 'd').valueOf()
       let interval = moment.duration(60, 'minutes').as('minutes')
       ecsMetric.getCpuTotal(instanceId, startTime, endTime, interval).then(res => {
-        console.log(res.data)
-        this.drawLine('cpuTotalChart', 'CPU总使用率', res.data)
+        // console.log(res.data)
+        // this.drawLine(this.cpuTotal, 'CPU总使用率', res.data)
+        let dataNames = ['maximum', 'minimum', 'average']
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLines(this.cpuTotal, this.getChartOption('CPU总使用率', dataNames, lines, this.mapXTimes(res.data)))
       })
       ecsMetric.getCpuSystem(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('cpuSystemChart', 'CPU内核空间使用率', res.data)
+        this.drawLine(this.cpuSystem, 'CPU内核空间使用率', res.data)
       })
       ecsMetric.getCpuUser(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('cpuUserChart', 'CPU用户空间使用率', res.data)
+        this.drawLine(this.cpuUser, 'CPU用户空间使用率', res.data)
       })
       ecsMetric.getLoad1m(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('load1MChart', '系统平均负载(1分钟内)', res.data)
+        this.drawLine(this.load1M, '系统平均负载(1分钟内)', res.data)
       })
       ecsMetric.getLoad5m(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('load5MChart', '系统平均负载(5分钟内)', res.data)
+        this.drawLine(this.load5M, '系统平均负载(5分钟内)', res.data)
       })
       ecsMetric.getLoad15m(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('load15MChart', '系统平均负载(15分钟内)', res.data)
+        this.drawLine(this.load15M, '系统平均负载(15分钟内)', res.data)
       })
       ecsMetric.getMemory(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('memoryChart', '内存使用率', res.data)
+        this.drawLine(this.memory, '内存使用率', res.data)
       })
       ecsMetric.getDiskInode(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('diskInodeChart', '磁盘inode使用率', res.data)
+        this.drawLine(this.diskInode, '磁盘inode使用率', res.data)
       })
       ecsMetric.getDiskReadRate(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('diskRRateChart', '磁盘读速率(Byte/s)', res.data)
+        this.drawLine(this.diskRRate, '磁盘读速率(Byte/s)', res.data)
       })
       ecsMetric.getDiskWriteRate(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('diskWRateChart', '磁盘写速率(Byte/s)', res.data)
+        this.drawLine(this.diskWRate, '磁盘写速率(Byte/s)', res.data)
       })
       ecsMetric.getDiskReadIOPS(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('diskRIopsChart', '磁盘读IOPS', res.data)
+        this.drawLine(this.diskRIops, '磁盘读IOPS', res.data)
       })
       ecsMetric.getDiskWriteIOPS(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('diskWIopsChart', '磁盘写IOPS', res.data)
+        this.drawLine(this.diskWIops, '磁盘写IOPS', res.data)
       })
       ecsMetric.getNetInBPS(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('netIRateChart', '下行带宽(bit/s)', res.data)
+        this.drawLine(this.netIRate, '下行带宽(bit/s)', res.data)
       })
       ecsMetric.getNetOutBPS(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('netORateChart', '上行带宽(bit/s)', res.data)
+        this.drawLine(this.netORate, '上行带宽(bit/s)', res.data)
       })
       ecsMetric.getNetInPPS(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('netIPpsChart', '网络流入数据包(packages/s)', res.data)
+        this.drawLine(this.netIPps, '网络流入数据包(packages/s)', res.data)
       })
       ecsMetric.getNetOutPPS(instanceId, startTime, endTime, interval).then(res => {
-        this.drawLine('netOPpsChart', '网络流出数据包(packages/s)', res.data)
+        this.drawLine(this.netOPps, '网络流出数据包(packages/s)', res.data)
       })
       ecsMetric.getTcpConnection(instanceId, startTime, endTime, interval).then(res => {
         // this.drawLine('tcpConnectionChart', 'TCP连接数', res.data)
-        this.drawTcpConnection('tcpConnectionChart', 'TCP连接数', res.data)
+        // this.drawTcpConnection(this.tcpConnection, 'TCP连接数', res.data)
+        let dataNames = ['TCP_TOTAL', 'ESTABLISHED', 'NON_ESTABLISHED']
+        let total = res.data.filter(function (item) {
+          return item.state === 'TCP_TOTAL'
+        }).map(function (item) {
+          return item.average
+        })
+        let estb = res.data.filter(function (item) {
+          return item.state === 'ESTABLISHED'
+        }).map(function (item) {
+          return item.average
+        })
+        let nestb = res.data.filter(function (item, index, arr) {
+          return item.state === 'NON_ESTABLISHED'
+        }).map(function (item) {
+          return item.average
+        })
+        let times = res.data.filter(function (item, index, arr) {
+          return index % 3 === 0
+        }).map(function (item) {
+          return moment(item.timestamp).format('YYYY/MM/DD HH:mm')
+        })
+        let lines = this.getLines(dataNames, [total, estb, nestb])
+        this.drawLines(this.tcpConnection, this.getChartOption('TCP连接数', dataNames, lines, times))
       })
     },
-    drawLine (chartId, chartName, metricArray) {
-      let myChart = this.$echarts.init(document.getElementById(chartId))
-      myChart.showLoading({
-        text: '正在加载'
-      })
+    getLines (names, dataPoints) {
+      let series = []
+      for (let i = 0; i < names.length; i++) {
+        series.push({
+          name: names[i],
+          type: 'line',
+          data: dataPoints[i]
+        })
+      }
+      console.log(series)
+      return series
+    },
+    getChartOption (chartName, dataNames, series, xTime) {
+      return {
+        backgroundColor: '#21202D',
+        legend: {
+          data: dataNames,
+          inactiveColor: '#777',
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        title: {
+          text: chartName,
+          textStyle: {
+            color: '#fff',
+            fontSize: 12
+          }
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            animation: false,
+            type: 'cross',
+            lineStyle: {
+              color: '#376df4',
+              width: 2,
+              opacity: 1
+            }
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: xTime,
+          axisLine: {lineStyle: {color: '#8392A5'}}
+        },
+        yAxis: {
+          scale: true,
+          axisLine: {lineStyle: {color: '#8392A5'}},
+          splitLine: {show: true}
+        },
+        grid: {
+          bottom: 80
+        },
+        dataZoom: [{
+          textStyle: {
+            color: '#8392A5'
+          },
+          handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+          handleSize: '80%',
+          dataBackground: {
+            areaStyle: {
+              color: '#8392A5'
+            },
+            lineStyle: {
+              opacity: 0.8,
+              color: '#8392A5'
+            }
+          },
+          handleStyle: {
+            color: '#fff',
+            shadowBlur: 3,
+            shadowColor: 'rgba(0, 0, 0, 0.6)',
+            shadowOffsetX: 2,
+            shadowOffsetY: 2
+          }
+        }, {
+          type: 'inside'
+        }],
+        series: series
+      }
+    },
+    drawLines (chart, option) {
+      chart.setOption(option)
+      chart.hideLoading()
+    },
+    drawLine (chart, chartName, metricArray) {
       let max = metricArray.map(function (item) {
         return item['maximum']
       })
@@ -274,14 +486,10 @@ export default {
           }
         ]
       }
-      myChart.setOption(option)
-      myChart.hideLoading()
+      chart.setOption(option)
+      chart.hideLoading()
     },
-    drawTcpConnection (chartId, chartName, metricArray) {
-      let myChart = this.$echarts.init(document.getElementById(chartId))
-      myChart.showLoading({
-        text: '正在加载'
-      })
+    drawTcpConnection (chart, chartName, metricArray) {
       let total = metricArray.filter(function (item) {
         return item.state === 'TCP_TOTAL'
       }).map(function (item) {
@@ -386,8 +594,8 @@ export default {
           }
         ]
       }
-      myChart.setOption(option)
-      myChart.hideLoading()
+      chart.setOption(option)
+      chart.hideLoading()
     }
   }
 }
@@ -397,6 +605,7 @@ export default {
   .titleTab {
     padding: 10px 0 10px 10px;
     background-color: #efefef;
+    width: 800px;
   }
   .chartGroup {
     /*width: 100%;*/
--- src/components/statistic/OssMonitor.vue
+++ src/components/statistic/OssMonitor.vue
@@ -14,18 +14,35 @@
 <!--      </select>-->
 <!--    </label>-->
     <el-card class="box-card chartGroup">
-      <div>
-        <div class="group1">
-          <div id="storageUtilChart" style="width: 500px;height:400px;"></div>
-          <div id="getRequestChart" style="width: 500px;height:400px;"></div>
-        </div>
-        <div class="group2">
-          <div id="putRequestChart" style="width: 500px;height:400px;"></div>
-          <div id="internetTXChart" style="width: 500px;height:400px;"></div>
-        </div>
+      <div class="titleTab">计量数据</div>
+      <div class="group1">
+        <div id="storageUtilChart" style="width: 800px;height:300px;"></div>
+      </div>
+      <div class="group2">
+        <div id="getRequestChart" style="width: 400px;height:300px;"></div>
+        <div id="putRequestChart" style="width: 400px;height:300px;"></div>
       </div>
       <div class="group3">
-        <div id="internetRXChart" style="width: 500px;height:400px;"></div>
+        <div id="internetRXChart" style="width: 400px;height:300px;"></div>
+        <div id="internetTXChart" style="width: 400px;height:300px;"></div>
+      </div>
+    </el-card>
+    <el-card class="box-card chartGroup">
+      <div class="titleTab">用户层级数据</div>
+      <div class="group1">
+        <div id="userRequestChart" style="width: 800px;height:300px;"></div>
+      </div>
+      <div class="group2">
+        <div id="userTrafficChart" style="width: 800px;height:300px;"></div>
+      </div>
+    </el-card>
+    <el-card class="box-card chartGroup">
+      <div class="titleTab">Bucket层级数据</div>
+      <div class="group1">
+        <div id="bucketRequestChart" style="width: 800px;height:300px;"></div>
+      </div>
+      <div class="group2">
+        <div id="bucketTrafficChart" style="width: 800px;height:300px;"></div>
       </div>
     </el-card>
   </div>
@@ -35,8 +52,13 @@
 import ossInfo from '@/api/oss/ossInfo'
 import ossMetric from '@/api/oss/ossMetric'
 import moment from 'moment'
+
 export default {
   name: 'ossMonitor',
+  mounted () {
+    this.initCharts()
+    this.showLoading()
+  },
   created () {
     ossInfo.getBuckets().then(res => {
       this.buckets = res.data
@@ -47,45 +69,169 @@ export default {
   data () {
     return {
       buckets: [],
-      bucketName: ''
+      bucketName: '',
+      storageUtil: {},
+      getRequest: {},
+      putRequest: {},
+      internetRX: {},
+      internetTX: {},
+      userRequest: {},
+      userTraffic: {},
+      bucketRequest: {},
+      bucketTraffic: {}
     }
   },
   methods: {
+    initCharts () {
+      this.storageUtil = this.$echarts.init(document.getElementById('storageUtilChart'))
+      this.getRequest = this.$echarts.init(document.getElementById('getRequestChart'))
+      this.putRequest = this.$echarts.init(document.getElementById('putRequestChart'))
+      this.internetRX = this.$echarts.init(document.getElementById('internetRXChart'))
+      this.internetTX = this.$echarts.init(document.getElementById('internetTXChart'))
+      this.userRequest = this.$echarts.init(document.getElementById('userRequestChart'))
+      this.userTraffic = this.$echarts.init(document.getElementById('userTrafficChart'))
+      this.bucketRequest = this.$echarts.init(document.getElementById('bucketRequestChart'))
+      this.bucketTraffic = this.$echarts.init(document.getElementById('bucketTrafficChart'))
+    },
+    showLoading () {
+      this.storageUtil.showLoading({
+        text: '正在加载'
+      })
+      this.getRequest.showLoading({
+        text: '正在加载'
+      })
+      this.putRequest.showLoading({
+        text: '正在加载'
+      })
+      this.internetRX.showLoading({
+        text: '正在加载'
+      })
+      this.internetTX.showLoading({
+        text: '正在加载'
+      })
+      this.userRequest.showLoading({
+        text: '正在加载'
+      })
+      this.userTraffic.showLoading({
+        text: '正在加载'
+      })
+      this.bucketRequest.showLoading({
+        text: '正在加载'
+      })
+      this.bucketTraffic.showLoading({
+        text: '正在加载'
+      })
+    },
+    mapDataPoints (resData, dataNames) {
+      let dataPoints = []
+      for (let i = 0; i < dataNames.length; i++) {
+        dataPoints.push(resData.map(function (item) {
+          return item[dataNames[i]]
+        }))
+      }
+      return dataPoints
+    },
+    mapXTimes (resData) {
+      return resData.map(function (item) {
+        return moment(item['timestamp']).format('YYYY/MM/DD HH:mm')
+      })
+    },
     refreshData (bucketName) {
+      this.showLoading()
       let endTime = new Date().valueOf()
       let startTime = moment(endTime).add(-50, 'd').valueOf()
       let interval = moment.duration(5, 'minutes').as('minutes')
       ossMetric.getStorageUtil(startTime, endTime, interval, bucketName).then(res => {
-        this.drawLine('storageUtilChart', '存储用量(B)', res.data, 'meteringStorageUtilization')
+        let dataNames = ['meteringStorageUtilization']
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.storageUtil, this.getChartOption('存储用量(B)', dataNames, lines, this.mapXTimes(res.data)))
       })
       ossMetric.getGetRequest(startTime, endTime, interval, bucketName).then(res => {
-        this.drawLine('getRequestChart', 'Get请求数', res.data, 'meteringGetRequest')
+        let dataNames = ['meteringGetRequest']
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.getRequest, this.getChartOption('Get请求数', dataNames, lines, this.mapXTimes(res.data)))
       })
       ossMetric.getPutRequest(startTime, endTime, interval, bucketName).then(res => {
-        this.drawLine('putRequestChart', 'Put请求数', res.data, 'meteringPutRequest')
+        let dataNames = ['meteringPutRequest']
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.putRequest, this.getChartOption('Put请求数', dataNames, lines, this.mapXTimes(res.data)))
       })
       ossMetric.getInternetTX(startTime, endTime, interval, bucketName).then(res => {
-        this.drawLine('internetTXChart', '公网流出流量(B)', res.data, 'meteringInternetTX')
+        let dataNames = ['meteringInternetTX']
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.internetTX, this.getChartOption('公网流出流量(B)', dataNames, lines, this.mapXTimes(res.data)))
       })
       ossMetric.getInternetRX(startTime, endTime, interval, bucketName).then(res => {
-        this.drawLine('internetRXChart', '公网流入流量(B)', res.data, 'meteringInternetRX')
+        let dataNames = ['meteringInternetRX']
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.internetRX, this.getChartOption('公网流入流量(B)', dataNames, lines, this.mapXTimes(res.data)))
+      })
+      ossMetric.getUserRequest(startTime, endTime, interval).then(res => {
+        let dataNames = [
+          'userTotalRequestCount', 'userSuccessRate',
+          'userServerErrorRate', 'userNetworkErrorRate',
+          'userAuthorizationErrorRate', 'userResourceNotFoundErrorRate',
+          'userClientTimeoutErrorRate', 'userClientOtherErrorRate'
+        ]
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.userRequest, this.getChartOption('用户层级请求详情', dataNames, lines, this.mapXTimes(res.data)))
+      })
+      ossMetric.getUserTraffic(startTime, endTime, interval).then(res => {
+        let dataNames = [
+          'userInternetSend', 'userInternetRecv',
+          'userIntranetSend', 'userIntranetRecv',
+          'userCdnSend', 'userCdnRecv',
+          'userSyncSend', 'userSyncRecv'
+        ]
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.userTraffic, this.getChartOption('用户层级流量详情', dataNames, lines, this.mapXTimes(res.data)))
+      })
+      ossMetric.getBucketRequest(startTime, endTime, interval, bucketName).then(res => {
+        let dataNames = [
+          'totalRequestCount', 'successRate',
+          'serverErrorRate', 'networkErrorRate',
+          'authorizationErrorRate', 'resourceNotFoundErrorRate',
+          'clientTimeoutErrorRate', 'clientOtherErrorRate'
+        ]
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.bucketRequest, this.getChartOption('bucket层级请求详情', dataNames, lines, this.mapXTimes(res.data)))
+      })
+      ossMetric.getBucketTraffic(startTime, endTime, interval, bucketName).then(res => {
+        let dataNames = [
+          'internetSend', 'internetRecv',
+          'intranetSend', 'intranetRecv',
+          'cdnSend', 'cdnRecv',
+          'syncSend', 'syncRecv'
+        ]
+        let dataPoints = this.mapDataPoints(res.data, dataNames)
+        let lines = this.getLines(dataNames, dataPoints)
+        this.drawLine(this.bucketTraffic, this.getChartOption('bucket层级流量详情', dataNames, lines, this.mapXTimes(res.data)))
       })
     },
-    drawLine (chartId, chartName, metricArray, metricName) {
-      let myChart = this.$echarts.init(document.getElementById(chartId))
-      myChart.showLoading({
-        text: '正在加载'
-      })
-      let result = metricArray.map(function (item) {
-        return item[metricName]
-      })
-      let times = metricArray.map(function (item) {
-        return moment(item['timestamp']).format('YYYY/MM/DD HH:mm')
-      })
-      let option = {
+    getLines (names, dataPoints) {
+      let series = []
+      for (let i = 0; i < names.length; i++) {
+        series.push({
+          name: names[i],
+          type: 'line',
+          data: dataPoints[i]
+        })
+      }
+      return series
+    },
+    getChartOption (chartName, dataNames, series, xTime) {
+      return {
         backgroundColor: '#21202D',
         legend: {
-          data: [metricName],
+          data: dataNames,
           inactiveColor: '#777',
           textStyle: {
             color: '#fff'
@@ -112,13 +258,13 @@ export default {
         },
         xAxis: {
           type: 'category',
-          data: times,
-          axisLine: { lineStyle: { color: '#8392A5' } }
+          data: xTime,
+          axisLine: {lineStyle: {color: '#8392A5'}}
         },
         yAxis: {
           scale: true,
-          axisLine: { lineStyle: { color: '#8392A5' } },
-          splitLine: { show: true }
+          axisLine: {lineStyle: {color: '#8392A5'}},
+          splitLine: {show: true}
         },
         grid: {
           bottom: 80
@@ -148,22 +294,23 @@ export default {
         }, {
           type: 'inside'
         }],
-        series: [
-          {
-            name: metricName,
-            type: 'line',
-            data: result
-          }
-        ]
+        series: series
       }
-      myChart.setOption(option)
-      myChart.hideLoading()
+    },
+    drawLine (chart, option) {
+      chart.setOption(option)
+      chart.hideLoading()
     }
   }
 }
 </script>
 
 <style scoped>
+  .titleTab {
+    padding: 10px 0 10px 10px;
+    background-color: #efefef;
+    width: 800px;
+  }
   .chartGroup {
     /*width: 100%;*/
     display: flex;
